c6716e05ca5f40a2ba17b0f79196e35f70e18d3c
Yuan <c14016057@gmail.com>
2017-05-04 23:48:46 +0800

    first

--- hw3/hw3_train.py
+++ hw3/hw3_train.py
@@ -1,5 +1,6 @@
 import numpy as np
 import sys
+import pandas as pd
 from numpy import genfromtxt
 from keras.models import Sequential
 from keras.layers.core import Dense,Dropout,Activation
@@ -7,99 +8,43 @@ from keras.layers import Conv2D,MaxPooling2D,Flatten
 from keras.optimizers import SGD,Adam,RMSprop
 from keras.utils import np_utils
 from keras.models import load_model
-#f = open(sys.argv[1],'r')
-#newf = open('newtrain.csv','w')
-#print('create newtrain.csv...')
-#for line in f:
-#	if len(line)>100:
-#		newf.write(line.replace(',',' '))
-#newf.close()
+from keras.preprocessing.image import ImageDataGenerator
 
-print('newtrain.csv created...')
-print('get data...')
-trainD = genfromtxt('newtrain.csv',delimiter=' ')
-print('get data suc...')
-print('divide data...')
-x_train = trainD[:,1:]
-X_train = x_train/255.
-y_train = trainD[:,0]
-print('divide data suc...')
-print('xyshape')
-print(x_train.shape)
-print(y_train.shape)
-y_train = np_utils.to_categorical(y_train,7)
-
-
-#######
-print('two clayer 10 20 one softmax')
-model = Sequential()
-
-model.add(Conv2D(64,(3,3), input_shape=(48,48,1)))
-model.add(MaxPooling2D((2,2)))
-
-model.add(Conv2D(128,(3,3)))
-model.add(MaxPooling2D((2,2)))
+dim = 48*48
+def loadData():
+    traw = pd.read_csv(sys.argv[1], sep=',', delimiter=None, header=0)
+    trawPA = np.array(traw)
+    trows = trawPA.shape[0]
+    tfeatures = np.empty([trows, dim])
+    ids = np.array([])
+    
+    for i in range(trows):
+        row = trawPA[i, :]
+        ids = np.append(ids, row[0])
+        feat = row[1].split()
+        feat = list(map(int, feat))
+        tfeatures[i, :] = feat
+    tfeatures.astype('float32')
+    tfeatures = tfeatures/255
+    return [ids, tfeatures]
+[id_test, x_test] = loadData()
+#y_early = y_train[20000:,:]
+#x_early = x_train[20000:,:]
+#y_train = y_train[:20000,:]
+#x_train = x_train[:20000,:]
+#x_early = x_early.reshape(x_early.shape[0],48,48,1)
+#x_train = x_train.reshape(x_train.shape[0],48,48,1)
 
 
-#model.add(Conv2D(30,(4,4)))
-#model.add(MaxPooling2D((2,2)))
+model = load_model('model.h5')
 
-model.add(Flatten())
-model.add(Dense(units=256,activation='relu'))
-model.add(Dense(units=7,activation='softmax'))
-
-x_train = x_train.reshape(x_train.shape[0],48,48,1)
-opt = Adam()
-model.compile(loss='categorical_crossentropy',optimizer=opt,metrics=['accuracy'])
-model.fit(x_train,y_train,batch_size=100,epochs=20)
-model.save('model0.h5')
-score=model.evaluate(x_train,y_train)
-
-f = open(sys.argv[2],'r')
-newf = open('newtest.csv','w')
-print('create newtest.csv...')
-for line in f:
-	if len(line)>100:
-		newf.write(line.replace(',',' '))
-newf.close()
-
-inputD = genfromtxt('newtest.csv',delimiter=' ')
-x_test = inputD[:,1:]
 x_test = x_test.reshape(x_test.shape[0],48,48,1)
-
-model = load_model('model0.h5')
 result=model.predict(x_test)
 result = np.argmax(result,axis=1)
-out = open(sys.argv[3],'w')
+out = open(sys.argv[2],'w')
 out.write('id,label\n')
 i=0
 for x in result:
-	out.write("%d,%d\n" %(i,x))
-	i=i+1
-#print(score[1])
+    out.write("%d,%d\n" %(i,x))
+    i=i+1
 
-'''
-##########
-print('two clayer 10 20 one dence 7 softmax')
-model2 = Sequential()
-
-model2.add(Conv2D(3,(3,3), input_shape=(48,48,1)))
-model2.add(MaxPooling2D((2,2)))
-
-model2.add(Conv2D(10,(3,3)))
-model2.add(MaxPooling2D((2,2)))
-
-model2.add(Conv2D(20,(3,3)))
-model2.add(MaxPooling2D((2,2)))
-
-
-
-model2.add(Flatten())
-#model.add(Dense(units=7,activation='relu'))
-model2.add(Dense(units=7,activation='softmax'))
-
-model2.compile(loss='categorical_crossentropy',optimizer='adam',metrics=['accuracy'])
-model2.fit(x_train,y_train,batch_size=100,epochs=5)
-score=model2.evaluate(x_train,y_train)
-print(score[1])
-'''
--- hw3/hw3_train.sh
+++ hw3/hw3_train.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-python3 hw3_train.py "$1" "$2" "$3" 
+python3 hw3_train.py "$1" "$2" 
